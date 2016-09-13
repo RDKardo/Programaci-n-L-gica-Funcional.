71daa40684f29626c7bb29927616b9ba0f27fdfc
Ricardo Montaño <rdkardo@gmail.com>
2016-09-13 08:58:11 -0500

    Arbol genealogico.

---
+++ Arbol genealogoco.pl
@@ -0,0 +1,13 @@
+padre(ricardo,felipe).
+padre(felipe,ricardo).
+padre(felipe,karen).
+padre(felipe,brenda).
+
+
+padre(domingo,abigail).
+padre(abigail,ricardo).
+padre(abigail,karen).
+padre(abigail,brenda).
+
+padre(guadalupe,felipe).
+padre(piedad,abigail).
